04ab8d093bf2dbb0cc9e0fea5d34371c2f3d4efc
Elsharif Shaban <93492560+sherifshabans@users.noreply.github.com>
2025-02-10 23:58:36 +0200

    Set Data

---
+++ app/src/main/java/com/elsharif/dailyseventy/domain/Zakker.kt
@@ -0,0 +1,9 @@
+package com.elsharif.dailyseventy.domain
+
+data class Zakker(
+    val category :String,
+    val  count : Int,
+    val description : String,
+    val reference : String,
+    val content : String
+)
